9a7e0e43aaba16763eec76fb3e6b73aab3437e89
purian <purian@gmail.com>
2014-08-17 12:06:12 +0300

    Add support for omni auth for linkedin strategy

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,16 @@
 class Authentication < ActiveRecord::Base
+
   belongs_to :user
+
+  validates_presence_of :uid, :provider
+  validates_uniqueness_of :uid, :scope => :provider
+
+  def update_auth_credentials(omniauth)
+
+    self.oauth_token = omniauth.credentials.token
+    self.oauth_expires_at = Time.at(omniauth.credentials.expires_at) unless omniauth.credentials.expires_at.nil?
+    self.secret = omniauth.credentials.secret
+    self.save!
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,47 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :authentications
 
+  def apply_omniauth(omniauth)
+    self.email = omniauth.info.email if email.blank?
+    authentications.build(:provider => omniauth.provider, :uid => omniauth.uid)
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    if persisted? and authentications.empty?
+      super
+    elsif persisted?
+      false
+    elsif not persisted?
+      not authentications.any?
+    else
+      super
+    end
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
 
 
 
