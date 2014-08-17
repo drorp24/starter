578599d05d02e867e52e6cae7a9033a8fdc48d2b
purian <purian@gmail.com>
2014-08-17 18:58:06 +0300

    Add relations between user profiles to user and make this model polymorphic

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 
+  has_many :authentications
   has_one :general_profile
   has_one :professional_profile
-  has_many :authentications
+  has_many :user_profiles
+  has_many :profiles, :through => :user_profiles
 
   def apply_omniauth(omniauth)
     self.email = omniauth.info.email if email.blank?
